35d9e2af9fcdaeb07faa7876c0a1818512be2142
unknown <gaurav.v@appliedsni.com>
2019-06-25 11:04:01 +0530

    redux form implemented

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,8 @@
 import { combineReducers } from "redux";
-
+import { reducer as formReducer } from "redux-form"; // to wire up redux-form in our app and we are renaming it as form reducer.
 import authReducer from "./authReducer";
 
 export default combineReducers({
-  auth: authReducer
+  auth: authReducer,
+  form: formReducer
 });
