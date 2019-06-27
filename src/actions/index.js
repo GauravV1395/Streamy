6db238cd7bb9200bfc5c00d0e5674bf761e719a3
unknown <gaurav.v@appliedsni.com>
2019-06-27 15:01:15 +0530

    create stream, list stream and history setup

--- src/actions/index.js
+++ src/actions/index.js
@@ -8,6 +8,7 @@ import {
   EDIT_STREAM
 } from "./types";
 import streams from "../apis/streams";
+import history from "../history";
 
 export const signIn = userId => {
   //userId is being passed as an argument from the component Googleauth to this action creater when it is called inside the component.
@@ -23,10 +24,12 @@ export const signOut = () => {
   };
 };
 
-export const createStream = formValues => async dispatch => {
-  const response = await streams.post("/streams", formValues);
+export const createStream = formValues => async (dispatch, getState) => {
+  const { userId } = getState().auth;
+  const response = await streams.post("/streams", { ...formValues, userId });
 
   dispatch({ type: CREATE_STREAM, payload: response.data });
+  history.push("/");
 };
 
 export const fetchStreams = () => async dispatch => {
---
+++ src/history.js
@@ -0,0 +1,5 @@
+// we are importing this version of history of ours in the app.js file as history.
+
+import { createBrowserHistory } from "history"; // this package is installed directly along with react-router-dom;
+
+export default createBrowserHistory();
